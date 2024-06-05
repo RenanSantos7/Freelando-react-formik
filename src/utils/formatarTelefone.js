80dce08c8a38b8c2f2a6bb15d5f75b7014664b6b
Renan Santos <renansantos7@proton.me>
2024-06-05 09:23:56 -0300

    Aula 5

--- src/utils/formatarTelefone.js
+++ src/utils/formatarTelefone.js
@@ -2,6 +2,10 @@ export function formatarTelefone(valor) {
 	if (!valor) return
 
 	const telefone = valor.replace(/\D/g, '')
-
-	return `(${telefone.slice(0, 2)}) ${telefone.slice(2, 7)}-${telefone.slice(7)}`
+	
+	if (telefone.length > 10) {
+		return `(${telefone.slice(0, 2)}) ${telefone.slice(2, 7)}-${telefone.slice(7)}`
+	}
+	
+	return `(${telefone.slice(0, 2)}) ${telefone.slice(2, 6)}-${telefone.slice(6)}`
 }
